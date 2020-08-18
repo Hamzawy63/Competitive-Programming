baf4b3bb7692e86cbaa0c89dc2239cf9f3371f9f
Hamzawy63 <hamzahasssan835@gmail.com>
2020-08-19 01:25:48 +0200

    fixed stupid implementation of sqrt

--- SPOJ/RMQSQ sqrt decomposition solution.cpp
+++ SPOJ/RMQSQ sqrt decomposition solution.cpp
@@ -12,8 +12,9 @@ int main() {
     cin >> n ;
     vector<int> a(n);
     for (int i = 0; i < n; i++)cin >> a[i];
-    int block_size = sqrt(n);
-    int number_of_blocks = n / sqrt(n);
+    int block_size = (int)sqrt(n);
+    int number_of_blocks = n / block_size;
+    if(n % block_size != 0 ) number_of_blocks++ ;
     vector<int> blk;
     for (int i = 0; i < number_of_blocks; i++) {
         int res = INT_MAX;
@@ -26,8 +27,8 @@ int main() {
     while (q--) {
         int l, r;
         cin >> l >> r;
-        int bl1 = l / n;
-        int bl2 = r / n;
+        int bl1 = l / block_size;
+        int bl2 = r / block_size;
         int res = INT_MAX;
         if (bl1 == bl2) {
             for (int i = l; i <= r; i++) res = min(res, a[i]);
@@ -37,11 +38,11 @@ int main() {
         for (int i = l; i < (bl1 + 1) * block_size; i++) {
             res = min(res, a[i]);
         }
-        for (int i = bl1 + 1; i <= bl2; i++) {
+        for (int i = bl1 + 1; i < bl2; i++) {
             res = min(res, blk[i]);
         }
         for (int i = bl2 * block_size; i <= r; i++) {
-            res = min(res, blk[i]);
+            res = min(res, a[i]);
         }
         cout << res << endl;
     }
