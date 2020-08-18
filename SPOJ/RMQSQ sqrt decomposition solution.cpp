ec2a0994f5730a3e182050123811473840931b10
Hamzawy63 <hamzahasssan835@gmail.com>
2020-08-19 01:09:47 +0200

    more minimum range queries problems :)

---
+++ SPOJ/RMQSQ sqrt decomposition solution.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long int ll;
+typedef long double ld;
+#define EPS 0.000000001
+#define space " "
+const int maxN = 200000 + 2;
+
+int main() {
+    int n, q;
+    cin >> n ;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)cin >> a[i];
+    int block_size = sqrt(n);
+    int number_of_blocks = n / sqrt(n);
+    vector<int> blk;
+    for (int i = 0; i < number_of_blocks; i++) {
+        int res = INT_MAX;
+        for (int j = block_size * i; j < min(n, (i + 1) * block_size); j++) {
+            res = min(res, a[j]);
+        }
+        blk.push_back(res);
+    }
+    cin >> q;
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        int bl1 = l / n;
+        int bl2 = r / n;
+        int res = INT_MAX;
+        if (bl1 == bl2) {
+            for (int i = l; i <= r; i++) res = min(res, a[i]);
+            cout << res << endl;
+            continue;
+        }
+        for (int i = l; i < (bl1 + 1) * block_size; i++) {
+            res = min(res, a[i]);
+        }
+        for (int i = bl1 + 1; i <= bl2; i++) {
+            res = min(res, blk[i]);
+        }
+        for (int i = bl2 * block_size; i <= r; i++) {
+            res = min(res, blk[i]);
+        }
+        cout << res << endl;
+    }
+
+}
